e34f151deeb606b4f1e4905362ac157d483e087a
Jishnu PN <jishnupneel@gmail.com>
2025-01-24 12:34:36 +0530

    Added forms

--- copo/copoapp/urls.py
+++ copo/copoapp/urls.py
@@ -4,4 +4,13 @@ urlpatterns = [
      path('home/', views.HomeView.as_view(), name ='home'),
      path('logout/', views.LogoutView.as_view(), name ='logout'),
      path('add-faculty/', views.FacultyView.as_view(), name ='add-faculty'),
+     path('get-department/', views.DepartmentView.as_view(), name ='get-department'),
+     path("add-student/", views.add_student, name="add-student"),
+     path("get-programme/", views.get_programmes, name="get-programmes"),
+     path("get-level/", views.get_levels, name="get-levels"),
+     path("add-programme/", views.add_programme, name="add-programme"),
+     path("add-course/", views.add_course, name="add-course"),
+     path("get-faculty/", views.get_faculty, name="get-faculty"),
+     path("get-courses/", views.get_courses, name="get-ourses"),
+     path("add-batch/", views.add_batch, name="add-batch"),
 ]
--- copo/copoapp/views.py
+++ copo/copoapp/views.py
@@ -7,7 +7,8 @@ from django.http import JsonResponse
 import json
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.core.validators import validate_email
-from .models import Faculty, Department
+from .models import Faculty, Department,Student,Programme,Level,Course,Batch
+from django.views.decorators.csrf import csrf_exempt
 
 class HomeView(APIView):  
    permission_classes = (IsAuthenticated, )
@@ -66,3 +67,152 @@ class FacultyView(APIView):
         faculty.save()
 
         return Response({'message': 'Faculty member created successfully.'}, status=status.HTTP_201_CREATED)
+    
+class DepartmentView(APIView): 
+    def get(self,request):
+        departments = Department.objects.all()
+        department_data = [
+            {"dept_id": dept.dept_id, "dept_name": dept.dept_name} for dept in departments
+        ]
+        return JsonResponse(department_data, safe=False)
+
+@csrf_exempt
+def add_student(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        try:
+            programme = Programme.objects.get(programme_name=data["programme"])
+            student = Student.objects.create(
+                register_no=data["register_no"],
+                name=data["name"],
+                programme=programme,
+                year_of_admission=data["year_of_admission"],
+                phone_number=data["phone_number"],
+                email=data["email"]
+            )
+            return JsonResponse({"message": "Student added successfully."}, status=201)
+        except Programme.DoesNotExist:
+            return JsonResponse({"programme": "Invalid programme."}, status=400)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=400)
+    return JsonResponse({"error": "Invalid request method."}, status=405)
+
+
+def get_programmes(request):
+    try:
+        programmes = Programme.objects.all()
+        programmes_data = [
+            {
+                "programme_id": programme.programme_id,
+                "programme_name": programme.programme_name,
+            }
+            for programme in programmes
+        ]
+        return JsonResponse(programmes_data, safe=False, status=200)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=400)
+
+def get_levels(request):
+    try:
+        levels = Level.objects.all()
+        levels_data = [
+            {"level_id": level.level_id, "level_name": level.name}
+            for level in levels
+        ]
+        return JsonResponse(levels_data, safe=False, status=200)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=400)
+
+@csrf_exempt
+def add_programme(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        try:
+            dept = Department.objects.get(dept_id=data["dept"])
+            level = Level.objects.get(name=data["level"])
+            programme = Programme.objects.create(
+                programme_name=data["programme_name"],
+                dept=dept,
+                no_of_pos=data["no_of_pos"],
+                level=level,
+                duration=data["duration"]
+            )
+            return JsonResponse({"message": "Programme created successfully."}, status=201)
+        except Department.DoesNotExist:
+            return JsonResponse({"dept": "Invalid department."}, status=400)
+        except Level.DoesNotExist:
+            return JsonResponse({"level": "Invalid level."}, status=400)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=400)
+    return JsonResponse({"error": "Invalid request method."}, status=405)
+
+    
+@csrf_exempt
+def add_course(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        try:
+            dept = Department.objects.get(dept_id=data["dept"])
+            programme = Programme.objects.get(programme_id=data["programme"])
+            course = Course.objects.create(
+                course_code=data["course_code"],
+                dept=dept,
+                semester=data["semester"],
+                credits=data["credits"],
+                no_of_cos=data["no_of_cos"],
+                programme=programme,
+                syllabus_year=data["syllabus_year"]
+            )
+            return JsonResponse({"message": "Course created successfully."}, status=201)
+        except Department.DoesNotExist:
+            return JsonResponse({"dept": "Invalid department."}, status=400)
+        except Programme.DoesNotExist:
+            return JsonResponse({"programme": "Invalid programme."}, status=400)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=400)
+    return JsonResponse({"error": "Invalid request method."}, status=405)
+
+def get_faculty(request):
+    try:
+        faculty_list = Faculty.objects.all()
+        faculty_data = [
+            {"faculty_id": faculty.faculty_id, "name": faculty.name} for faculty in faculty_list
+        ]
+        return JsonResponse(faculty_data, safe=False, status=200)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=400)
+
+# Get all courses
+def get_courses(request):
+    try:
+        course_list = Course.objects.all()
+        course_data = [
+            {"course_id": course.course_id, "course_code": course.course_code} for course in course_list
+        ]
+        return JsonResponse(course_data, safe=False, status=200)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=400)
+
+
+@csrf_exempt
+def add_batch(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        try:
+            faculty = Faculty.objects.get(faculty_id=data["faculty_id"])
+            course = Course.objects.get(course_id=data["course"]) if data["course"] else None
+            batch = Batch.objects.create(
+                faculty_id=faculty,
+                course=course,
+                year=data["year"],
+                part=data["part"],
+                active=data["active"],
+            )
+            return JsonResponse({"message": "Batch created successfully."}, status=201)
+        except Faculty.DoesNotExist:
+            return JsonResponse({"faculty_id": "Invalid faculty."}, status=400)
+        except Course.DoesNotExist:
+            return JsonResponse({"course": "Invalid course."}, status=400)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=400)
+    return JsonResponse({"error": "Invalid request method."}, status=405)
